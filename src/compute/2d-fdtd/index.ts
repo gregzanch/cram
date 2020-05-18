ea40f28589f238fc84e7978effe18f75bdbfc95c
Greg Zanchelli <zanchelli.greg@gmail.com>
2020-05-17 23:04:16 -0400

    made grid sizing more intuitive

--- src/compute/2d-fdtd/index.ts
+++ src/compute/2d-fdtd/index.ts
@@ -1,17 +1,25 @@
 import Renderer from "../../render/renderer";
 import Messenger from "../../messenger";
-import { PlaneBufferGeometry, ShaderMaterial, UniformsLib, Mesh, WebGLRenderer, DataTexture, UniformsUtils, Color, CanvasTexture } from 'three';
+import { PlaneBufferGeometry, ShaderMaterial, UniformsLib, Mesh, WebGLRenderer, DataTexture, UniformsUtils, Color, DoubleSide } from 'three';
 import { GPUComputationRenderer, Variable } from "three/examples/jsm/misc/GPUComputationRenderer.js";
 import shaders from './shaders';
 import Solver from "../solver";
 import FDTDSource from "./fdtd-source";
+import map from '../../common/map';
 
-const TWOPI = Math.PI * 2;
 
+
+const defaults = {
+  width: 10,
+  height: 10
+};
 
 export interface FDTD_2D_Props{
   messenger: Messenger;
   renderer: Renderer;
+  width?: number;
+  height?: number;
+  cellsize?: number;
 }
 
 export interface Uniforms  {
@@ -26,21 +34,23 @@ class FDTD_2D extends Solver {
   messenger: Messenger;
   gpuCompute!: GPUComputationRenderer;
 
-  /**
-   * essentially the grid resolution
-   */
-  textureWidth: number;
 
   /**
-   * the plane's width
+   * number of x cells
    */
-  bounds: number;
+  nx: number;
+  
+  /**
+   * number of y cells
+   */
+  ny: number;
+  
+  
   uniforms!: Uniforms;
   mesh!: Mesh;
   heightmapVariable!: Variable;
   sourcemapVariable!: Variable;
-  sourcemap!: CanvasTexture;
-  sourcemapctx!: CanvasRenderingContext2D;
+  sourcemap!: DataTexture;
   readWaterLevelShader!: THREE.ShaderMaterial;
   readWaterLevelImage!: Uint8Array;
   readWaterLevelRenderTarget!: THREE.WebGLRenderTarget;
@@ -54,15 +64,29 @@ class FDTD_2D extends Solver {
    * simulation time step in seconds
    */
   dt: number;
+  width: number;
+  height: number;
+  cellsize: number;
   constructor(props: FDTD_2D_Props) {
     super(props);
+    this.kind = "fdtd-2d";
     this.messenger = props.messenger;
     this.renderer = props.renderer;
     this.dt = 0.25 / 1000;
     this.running = false;
     this.time = 0;
-    this.textureWidth = 128;
-    this.bounds = 128;
+    
+    const _width = props.width || defaults.width;
+    const _height = props.height || defaults.height;
+    this.cellsize = props.cellsize || Math.max(_width,_height)/128;
+    
+    this.nx = Math.ceil(_width / this.cellsize);
+    this.ny = Math.ceil(_height / this.cellsize);
+    
+    this.width = this.nx * this.cellsize;
+    this.height = this.ny * this.cellsize;
+    
+    
     this.sources = [] as FDTDSource[];
 
     this.fillTexture = this.fillTexture.bind(this);
@@ -91,15 +115,14 @@ class FDTD_2D extends Solver {
   init() {
     this.sources.push(
       new FDTDSource({
-        x: 15,
-        y: 15,
-        r: 3,
+        x: 10,
+        y: 10,
         amplitude: 1,
-        frequency: 1,
-        phase: 1
+        frequency: 50,
+        phase: 0
       })
     );
-    const geometry = new PlaneBufferGeometry(this.bounds, this.bounds, this.textureWidth - 1, this.textureWidth - 1);
+    const geometry = new PlaneBufferGeometry(this.width, this.height, this.nx - 1, this.ny - 1);
     const { common, specularmap, envmap, aomap, lightmap, fog } = UniformsLib;
     const heightmap = { value: null };
     const uniforms = UniformsUtils.merge([
@@ -119,35 +142,35 @@ class FDTD_2D extends Solver {
         emissive: { value: new Color(0x000000) },
         specular: { value: new Color(0x111111) },
         shininess: { value: 30 },
+        colorBrightness: { value: 10 },
+        cell_size: { value: this.cellsize },
+        inv_cell_size: { value: 1 / this.cellsize },
+        heightScale: { value: 0.1 },
         heightmap
       }
     ]);
     const vertexShader = shaders.waterVert;
     const fragmentShader = shaders.waterFrag;
-
-    const material = new ShaderMaterial({ uniforms, vertexShader, fragmentShader });
+    const side = DoubleSide;
+    const material = new ShaderMaterial({ uniforms, vertexShader, fragmentShader, side });
     material.lights = true;
-    material.defines.WIDTH = this.textureWidth.toFixed(1);
-    material.defines.BOUNDS = this.bounds.toFixed(1);
+
 
     this.uniforms = material.uniforms;
     this.mesh = new Mesh(geometry, material);
 
     this.mesh.matrixAutoUpdate = false;
     this.mesh.updateMatrix();
-    this.mesh.scale.setScalar(4 / this.textureWidth);
+    // this.mesh.scale.setScalar(this.width / this.nx);
     (this.mesh.material as ShaderMaterial).wireframe = false;
     this.mesh.matrixAutoUpdate = true;
+    this.mesh.position.setX(this.width / 2);
     this.renderer.env.add(this.mesh);
 
-    this.gpuCompute = new GPUComputationRenderer(this.textureWidth, this.textureWidth, this.renderer.renderer as WebGLRenderer);
+    this.gpuCompute = new GPUComputationRenderer(this.nx, this.ny, this.renderer.renderer as WebGLRenderer);
     let heightmapInit = this.gpuCompute.createTexture();
-    this.sourcemapctx = document.createElement("canvas").getContext("2d")!;
-    this.sourcemapctx.canvas.width = this.textureWidth;
-    this.sourcemapctx.canvas.height = this.textureWidth;
-    this.sourcemapctx.clearRect(0, 0, this.sourcemapctx.canvas.width, this.sourcemapctx.canvas.height);
-    this.sourcemap = new THREE.CanvasTexture(this.sourcemapctx.canvas);
-
+    this.sourcemap = this.gpuCompute.createTexture();
+    this.fillSourceTexture();
     this.updateSourceTexture();
     this.fillTexture(heightmapInit);
     this.heightmapVariable = this.gpuCompute.addVariable("heightmap", shaders.heightMapFrag, heightmapInit);
@@ -156,9 +179,10 @@ class FDTD_2D extends Solver {
     (this.heightmapVariable.material as THREE.ShaderMaterial).uniforms["sourcemap"] = { value: this.sourcemap };
     (this.heightmapVariable.material as THREE.ShaderMaterial).uniforms["mousePos"] = { value: new THREE.Vector2(5, 5) };
     (this.heightmapVariable.material as THREE.ShaderMaterial).uniforms["mouseSize"] = { value: 0.0 };
-    (this.heightmapVariable.material as THREE.ShaderMaterial).uniforms["viscosityConstant"] = { value: 0.98 };
+    (this.heightmapVariable.material as THREE.ShaderMaterial).uniforms["damping"] = { value: 0.9999 };
     (this.heightmapVariable.material as THREE.ShaderMaterial).uniforms["heightCompensation"] = { value: 0 };
-    (this.heightmapVariable.material as THREE.ShaderMaterial).defines.BOUNDS = this.bounds.toFixed(1);
+    (this.heightmapVariable.material as THREE.ShaderMaterial).uniforms["cell_size"] = { value: this.cellsize };
+    (this.heightmapVariable.material as THREE.ShaderMaterial).uniforms["inv_cell_size"] = { value: 1/this.cellsize };
 
     const error = this.gpuCompute.init();
     if (error !== null) {
@@ -167,10 +191,11 @@ class FDTD_2D extends Solver {
 
     this.readWaterLevelShader = this.gpuCompute["createShaderMaterial"](shaders.readWaterLevelFrag, {
       point1: { value: new THREE.Vector2() },
-      levelTexture: { value: null }
+      levelTexture: { value: null },
+      cell_size: { value: this.cellsize },
+      inv_cell_size: { value: 1/this.cellsize}
     });
-    this.readWaterLevelShader.defines.WIDTH = this.textureWidth.toFixed(1);
-    this.readWaterLevelShader.defines.BOUNDS = this.textureWidth.toFixed(1);
+
 
     // Create a 4x1 pixel image and a render target (Uint8, 4 channels, 1 byte per channel) to read water height and orientation
     this.readWaterLevelImage = new Uint8Array(4 * 1 * 4);
@@ -186,30 +211,58 @@ class FDTD_2D extends Solver {
       depthBuffer: false
     });
   }
-
+  clearSourceTexture() {}
+  fillSourceTexture() {
+    const pixels = this.sourcemap.image.data;
+    let p = 0;
+    for (let j = 0; j < this.ny; j++) {
+      for (let i = 0; i < this.nx; i++) {
+        const x = i / this.nx;
+        const y = j / this.ny;
+        const n = 3;
+        pixels[p + 0] = 0;
+        pixels[p + 1] = 0;
+        pixels[p + 2] = 0;
+        pixels[p + 3] = 1;
+        p += 4;
+      }
+    }
+  }
   updateSourceTexture() {
-    this.sourcemapctx.clearRect(0, 0, this.textureWidth, this.textureWidth);
-    for (let i = 0; i < this.sources.length; i++){
-      this.sourcemapctx.fillStyle = this.sources[i].fillStyle;
-      this.sourcemapctx.beginPath();
+    const pixels = this.sourcemap.image.data;
+    for (let i = 0; i < this.sources.length; i++) {
+      const index = 4 * (this.sources[i].y * this.nx + this.sources[i].x);
+      this.sources[i].update(this.time);
+      const value = this.sources[i].value;
+      const vel = this.sources[i].velocity;
+      pixels[index + 0] = map(value, -10, 10, 0, 255);
+      pixels[index + 1] = map(vel, -10, 10, 0, 255);
+      pixels[index + 3] = 0;
 
-      this.sourcemapctx.arc(this.sources[i].x, this.sources[i].y, this.sources[i].r, 0, TWOPI);
-      this.sourcemapctx.fill(); 
+      if (this.sources[i].shouldClearPreviousPosition) {
+        const previndex = 4 * (this.sources[i].previousY * this.nx + this.sources[i].previousX);
+        pixels[previndex + 0] = 0;
+        pixels[previndex + 1] = 0;
+        pixels[previndex + 3] = 1;
+        this.sources[i].shouldClearPreviousPosition = false;
+      }
     }
     this.sourcemap.needsUpdate = true;
-
   }
   fillTexture(texture: DataTexture) {
     const pixels = texture.image.data;
     let p = 0;
-    for (let j = 0; j < this.textureWidth; j++) {
-      for (let i = 0; i < this.textureWidth; i++) {
-        const x = i / this.textureWidth;
-        const y = j / this.textureWidth;
+    for (let j = 0; j < this.ny; j++) {
+      for (let i = 0; i < this.nx; i++) {
+        const x = i / this.nx;
+        const y = j / this.ny;
         const n = 3;
-        pixels[p + 0] = (Math.sin(n * Math.PI * x) * Math.cos(n * Math.PI * y) + 1) / 2;
-        pixels[p + 1] = pixels[p + 0];
-        pixels[p + 2] = 0;
+        // const value = Math.sin(n * Math.PI * x) * Math.cos(n * Math.PI * y);
+        const value = 0;
+
+        pixels[p + 0] = map(value, -10, 10, 0, 255);
+        pixels[p + 1] = 0;
+        pixels[p + 2] = 1;
         pixels[p + 3] = 1;
         p += 4;
       }
