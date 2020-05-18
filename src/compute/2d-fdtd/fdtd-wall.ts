0efe894db2712e17674eaf01e74970a41f50cf80
Greg Zanchelli <zanchelli.greg@gmail.com>
2020-05-18 16:31:46 -0400

    added walls to fdtd

---
+++ src/compute/2d-fdtd/fdtd-wall.ts
@@ -0,0 +1,43 @@
+import map from "../../common/map";
+import rasterizeLine from './rasterize-line';
+
+export interface FDTDWallProps {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+}
+
+class FDTDWall {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+  cells: number[][];
+  previousCells: number[][];
+  shouldClearPreviousCells: boolean;
+  constructor(props: FDTDWallProps) {
+    this.x1 = props.x1;
+    this.y1 = props.y1;
+    this.x2 = props.x2;
+    this.y2 = props.y2;
+    this.cells = rasterizeLine(this.x1, this.y1, this.x2, this.y2);
+    this.previousCells = this.cells;
+    this.shouldClearPreviousCells = false;
+    
+  }
+
+  move(props: FDTDWallProps) {
+    this.previousCells = this.cells;
+    this.x1 = props.x1;
+    this.y1 = props.y1;
+    this.x2 = props.x2;
+    this.y2 = props.y2;
+    this.cells = rasterizeLine(this.x1, this.y1, this.x2, this.y2);
+    this.shouldClearPreviousCells = true;
+  }
+}
+
+export { FDTDWall };
+
+export default FDTDWall;
