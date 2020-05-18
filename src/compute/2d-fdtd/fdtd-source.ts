34d3eb719d1343c253a98279235c03ad856934bd
Greg Zanchelli <zanchelli.greg@gmail.com>
2020-05-17 23:03:43 -0400

    fdtd-2d source move functionality

--- src/compute/2d-fdtd/fdtd-source.ts
+++ src/compute/2d-fdtd/fdtd-source.ts
@@ -1,7 +1,8 @@
+import map from '../../common/map';
+
 export interface FDTDSourceProps {
   x: number;
   y: number;
-  r: number;
   amplitude: number;
   frequency: number;
   phase: number;
@@ -10,28 +11,45 @@ export interface FDTDSourceProps {
 class FDTDSource{
   x: number;
   y: number;
-  r: number;
   amplitude: number;
   frequency: number;
   phase: number;
-  value!: number;
+  value: number;
+  previousValue: number;
+  velocity: number;
+  rgba: number[];
+  previousX: number;
+  previousY: number;
+  shouldClearPreviousPosition: boolean;
   constructor(props: FDTDSourceProps) {
     this.x = props.x;
     this.y = props.y;
-    this.r = props.r;
+    this.previousX = this.x;
+    this.previousY = this.y;
+    this.shouldClearPreviousPosition = false;
     this.amplitude = props.amplitude;
     this.frequency = props.frequency;
     this.phase = props.phase;
     this.value = 0;
-    this.update = this.update.bind(this);
+    this.previousValue = 0;
+    this.velocity = 0;
+    this.rgba = [0, 0, 0, 1];
     this.update = this.update.bind(this);
   }
   update(time: number) {
-    this.value = Math.round((this.amplitude * Math.sin(2 * Math.PI * this.frequency * time + this.phase) + 1) / 2 * 255);
-    return this.value;
+    this.previousValue = this.value;
+    this.value = this.amplitude * Math.sin(2 * Math.PI * this.frequency * time + this.phase);
+    this.velocity = this.value - this.previousValue;
+    this.rgba[0] = map(this.value, -100, 100, 0, 255);
+    this.rgba[1] = map(this.velocity, -100, 100, 0, 255);
+    this.rgba[3] = 0;
   }
-  get fillStyle() {
-    return "#"+this.value.toString(16) + "000000";
+  move(x: number, y: number) {
+    this.previousX = this.x;
+    this.previousY = this.y;
+    this.x = x;
+    this.y = y;
+    this.shouldClearPreviousPosition = true;
   }
 }
 
