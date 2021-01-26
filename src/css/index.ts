4cb89730173e8cf5a1de4b231cff665545157119
Greg Zanchelli <zanchelli.greg@gmail.com>
2021-01-25 23:59:55 -0500

    fix failed import

--- src/css/index.ts
+++ src/css/index.ts
@@ -3,7 +3,7 @@ import "normalize.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/table/lib/css/table.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
-import "~@blueprintjs/popover2/lib/css/blueprint-popover2.css";
+import "@blueprintjs/popover2/lib/css/blueprint-popover2.css";
 
 import "./global.css";
 
