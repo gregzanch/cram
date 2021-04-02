97bc7f50ac1483dce6eedb8ed6f3e1a1c79e03c0
Jack Breton <breton.jack.c@gmail.com>
2021-04-02 15:07:34 -0400

    filter changes

--- src/compute/raytracer/image-source/index.ts
+++ src/compute/raytracer/image-source/index.ts
@@ -671,27 +671,35 @@ export class ImageSourceSolver extends Solver {
           for(let f = 0; f<this.frequencies.length; f++){
             samples[f][roundedSample] += p[f]; 
           }
-
         }
           
-        samples.forEach((x,i,arr)=>{
-          const {b, a} = coefs.get(freqs[i])!;
-          arr[i] = filter(b,a,x);
-        });
+        // samples.forEach((x,i,arr)=>{
+        //   const {b, a} = coefs.get(freqs[i])!;
+        //   arr[i] = filter(b,a,x);
+        // });
     
         const offlineContext = audioEngine.createOfflineContext(1, endSample, sampleRate);
-        const sources = samples.map(x => audioEngine.createBufferSource(x, offlineContext));
+
+        const sources = Array(this.frequencies.length); 
+        for(let f = 0; f<this.frequencies.length; f++){
+          sources[f] = audioEngine.createFilteredSource(samples[f],this.frequencies[f],1.414,1,offlineContext); 
+        }
+
+        //const sources = samples.map(x => audioEngine.createBufferSource(x, offlineContext));
+
+        console.log(sources);
+
         const merger = audioEngine.createMerger(sources.length, offlineContext);
         
         for(let i = 0; i<sources.length; i++){
-          sources[i].connect(merger, 0, i);
+          sources[i].source.connect(merger, 0, i);
         }
     
         merger.connect(offlineContext.destination);
-        sources.forEach(source=>source.start());
-    
-        console.log(samples); 
+        sources.forEach(source=>source.source.start());
+
         this.impulseResponse = await offlineContext.startRendering();
+
         return this.impulseResponse;
 
       } 
